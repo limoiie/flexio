22a29c969bc7f53cc1c217f996365e0a607ddc04
limoiie <limo.iie4@gmail.com>
2022-12-05 20:51:20 +0800

    chore: Init __version__ in __init__.py

--- src/flexio/__init__.py
+++ src/flexio/__init__.py
@@ -1,3 +1,14 @@
-from .flexio import FlexBinaryIO, FlexTextIO
-
 __all__ = ['FlexBinaryIO', 'FlexTextIO']
+
+try:
+    import importlib.metadata as _importlib_metadata
+except ModuleNotFoundError:
+    # noinspection PyUnresolvedReferences
+    import importlib_metadata as _importlib_metadata
+
+try:
+    __version__ = _importlib_metadata.version("flexio")
+except _importlib_metadata.PackageNotFoundError:
+    __version__ = "unknown version"
+
+from .flexio import FlexBinaryIO, FlexTextIO
