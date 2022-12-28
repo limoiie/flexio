71a42abc360f3b92bb9caa05b8814a75420267bd
limoiie <limo.iie4@gmail.com>
2022-12-28 22:12:41 +0800

    refactor: Remove FlexBinaryIO and FlexTextIO out off __init__
    This change is for encouraging to use method flex_open

--- src/flexio/__init__.py
+++ src/flexio/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['FlexBinaryIO', 'FlexTextIO', 'FilePointer', 'flex_open']
+__all__ = ['FilePointer', 'flex_open']
 
 try:
     import importlib.metadata as _importlib_metadata
@@ -11,4 +11,4 @@ try:
 except _importlib_metadata.PackageNotFoundError:
     __version__ = "unknown version"
 
-from .flexio import FilePointer, FlexBinaryIO, FlexTextIO, flex_open
+from .flexio import FilePointer, flex_open
